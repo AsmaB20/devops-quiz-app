d72744ecebb762baa499d7ae1cbdf6c055af66df
Reham123905 <60106294@udst.edu.qa>
2026-02-04 09:44:21 +0300

    Display quiz questions and options

--- app.js
+++ app.js
@@ -24,3 +24,34 @@ function loadTopics() {
     select.appendChild(option)
   }
 }
+
+document.getElementById("topicSelect").addEventListener("change", function () {
+  let selectedTopic = this.value
+  let filtered = []
+
+  for (let i = 0; i < questions.length; i++) {
+    if (questions[i].topic === selectedTopic) {
+      filtered.push(questions[i])
+    }
+  }
+
+  showQuestion(filtered[0])
+})
+
+function showQuestion(q) {
+  let quizDiv = document.getElementById("quiz")
+  quizDiv.innerHTML = ""
+
+  let question = document.createElement("h3")
+  question.textContent = q.question
+  quizDiv.appendChild(question)
+
+  for (let i = 0; i < q.options.length; i++) {
+    let btn = document.createElement("button")
+    btn.textContent = q.options[i]
+    btn.onclick = function () {
+      checkAnswer(i, q)
+    }
+    quizDiv.appendChild(btn)
+  }
+}
